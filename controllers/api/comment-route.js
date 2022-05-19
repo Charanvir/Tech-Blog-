5ffc76c8c02f66ea8be1bcc403efacf826d3b1f5
Charanvir <charanvir123@gmail.com>
2022-05-19 02:50:59 -0400

    created all CRUD functions for Comments data

--- controllers/api/comment-route.js
+++ controllers/api/comment-route.js
@@ -23,6 +23,90 @@ router.get("/", (req, res) => {
             console.log(err)
             res.status(500).json(err)
         })
+});
+
+router.get("/:id", (req, res) => {
+    Comment.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'comment_text'
+        ],
+        include: [
+            {
+                model: User,
+                attributes: ['user']
+            },
+            {
+                model: Post,
+                attributes: ['title']
+            }
+        ]
+    })
+        .then(commentData => {
+            if (!commentData) {
+                res.status(404).json({ message: 'No comment with this ID was found' });
+                return
+            }
+            res.json(commentData)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json(err)
+        })
+});
+
+router.post('/', (req, res) => {
+    Comment.create({
+        user_id: req.body.user_id,
+        post_id: req.body.post_id,
+        comment_text: req.body.comment_text
+    })
+        .then(commentData => res.json(commentData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+router.put('/:id', (req, res) => {
+    Comment.update(req.body, {
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(commentData => {
+            if (!commentData) {
+                res.status(404).json({ message: 'No comment with this ID was found' });
+                return
+            }
+            res.json(commentData)
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+router.delete("/:id", (req, res) => {
+    Comment.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(commentData => {
+            if (!commentData) {
+                res.status(404).json({ message: 'No comment with this ID was found' });
+                return
+            }
+            res.json(commentData)
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 })
 
 module.exports = router;
