bde7dad783c253a3e993c1c91e7111b77fb8822a
Charanvir <charanvir123@gmail.com>
2022-05-20 00:39:01 -0400

    added all post data into homepage

--- controllers/homeroute.js
+++ controllers/homeroute.js
@@ -3,7 +3,38 @@ const sequelize = require("../config/connection")
 const { User, Post, Comment } = require('../models')
 
 router.get("/", (req, res) => {
-    res.render('homepage')
+    Post.findAll({
+        attributes: [
+            'title',
+            'created_at',
+            'id'
+        ],
+        include: [
+            {
+                model: User,
+                attributes: [
+                    'user'
+                ]
+            }
+        ]
+    })
+        .then(postData => {
+            const posts = postData.map(post => post.get({ plain: true }));
+            res.render('homepage', { posts })
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json(err)
+        })
+
+});
+
+router.get('/login', (req, res) => {
+    res.render('login')
+});
+
+router.get('/dashboard', (req, res) => {
+    res.render('dashboard')
 })
 
 module.exports = router;
