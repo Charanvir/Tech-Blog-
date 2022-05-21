92c2335016221262ad9e661d8a8e23f4b709f1c6
Charanvir <charanvir123@gmail.com>
2022-05-21 02:26:33 -0400

    created dashboard route with working log status

---
+++ controllers/dashboard.js
@@ -0,0 +1,44 @@
+const router = require('express').Router();
+const { User, Post, Comment } = require("../models")
+const authorization = require('../utils/authorization')
+
+router.get("/", authorization, (req, res) => {
+    console.log(req.session.user_id)
+    Post.findAll({
+        where: {
+            user_id: req.session.user_id
+        },
+        attributes: [
+            'title',
+            'created_at',
+            'id',
+            'post_text'
+        ],
+        include: [
+            {
+                model: User,
+                attributes: [
+                    'user'
+                ]
+            },
+            {
+                model: Comment,
+                attributes: ['comment_text'],
+                include: {
+                    model: User,
+                    attributes: ['user']
+                }
+            }
+        ]
+    })
+        .then(postData => {
+            const posts = postData.map(post => post.get({ plain: true }));
+            res.render('dashboard', { posts, loggedIn: true })
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json(err)
+        })
+})
+
+module.exports = router
--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,10 @@ const router = require("express").Router();
 
 const apiRoutes = require("./api/");
 const homeRoute = require("./homeroute")
+const dashboardRoute = require("./dashboard")
 
 router.use('/', homeRoute)
 router.use('/api', apiRoutes);
+router.use("/dashboard", dashboardRoute)
 
 module.exports = router
