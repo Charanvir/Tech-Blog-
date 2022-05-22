a7ee28cf1f9a318ccd7750fb081797e0774a6ce0
Charanvir <charanvir123@gmail.com>
2022-05-22 01:00:10 -0400

    added functionality to edit a post created by the logged in user

--- public/js/deleteEditPost.js
+++ public/js/deleteEditPost.js
@@ -18,7 +18,26 @@ async function deletePost(event) {
 async function editPost(event) {
     event.preventDefault();
     const post_id = window.location.pathname.split('/')[window.location.pathname.split('/').length - 1]
-    console.log(post_id)
+    const title = document.querySelector('#editTitle').value.trim();
+    const post_text = document.querySelector('#editContent').value.trim();
+    if (title && post_text) {
+        const editedPost = await fetch(`/api/posts/${post_id}`, {
+            method: 'PUT',
+            body: JSON.stringify({
+                title, post_text
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        if (editedPost.ok) {
+            document.location.reload();
+        } else {
+            alert("Post update failed, please try again")
+        }
+
+    }
 }
 
 document.querySelector("#deleteBtn").addEventListener('click', deletePost);
